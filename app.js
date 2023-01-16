60d859a8d7f5e937f417789f3582851de1c47f6a
Pierre Barbé <pierrebarbe0@gmail.com>
2023-01-16 17:09:50 +0100

    modif

--- app.js
+++ app.js
@@ -4,34 +4,56 @@ import mariadb from 'mariadb';
 
 const app = express();
 const port = 3001;
+const validToken = "auboulot";
 
-const connection = mariadb.createConnection({
-host: 'localhost',
-user: 'admin_serre',
-password: '132456',
-database: 'data_serre'
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers['x-access-token'];
+  if (!token) {
+    return res.status(401).send('Unauthorized request');
+  }
+
+  if (token !== validToken) {
+    return res.status(400).send('Invalid token');
+  }
+
+  next();
+};
+
+/* const pool = mariadb.createPool({
+  host: 'localhost',
+  user: 'admin_serre',
+  password: '132456',
+  database: 'data_serre',
+}); */
+
+const pool = mariadb.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'data_serre',
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-res.send('Hello World fdp!');
+  res.send('Hello World fdp!');
 });
 
-app.post('/statistique', (req, res) => {
-const { startDate, endDate } = req.body;
-const query = SELECT * FROM data_serre WHERE timestamp BETWEEN '${startDate}' AND '${endDate}';
-connection.query(query
-, (err, results) => {
-if (err) {
-res.status(500).send('Erreur lors de la récupération des statistiques');
-} else {
-res.json(results);
-}
-});
+app.post('/statistique', /* verifyToken, */ (req, res) => {
+  const { startDate, endDate } = req.body;
+  const query = `SELECT * FROM stats`;
+  pool.query(query
+    , (err, results) => {
+      if (err) {
+        res.status(500).send('Erreur lors de la récupération des statistiques');
+      } else {
+        console.log("retour");
+        res.json(results);
+      }
+    });
 });
 
 app.listen(port, () => {
-return console.log(Express is listening at http://localhost:${port});
+  return console.log("test");
 });
